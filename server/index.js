51c352e087a564b814e95cfea0d15c3283608397
Sudhir K <sudhirkannan2005@gmail.com>
2025-06-17 09:44:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'https://zap-chat-pteog4dxf-sudhirkannans-projects.vercel.app/',
+        origin: '*',
         methods: ['GET', 'POST'],
     },
 });
