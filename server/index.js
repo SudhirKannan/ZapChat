6b18fb3b77e87896f02f7265d196029d78bc7cba
Sudhir K <sudhirkannan2005@gmail.com>
2025-06-17 09:49:11 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: ["https://zap-chat-alpha.vercel.app","http://localhost:5173"],
         methods: ['GET', 'POST'],
     },
 });
